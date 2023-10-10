88bee3c8accb9da3c1da76e36d9e6dc8a0cb2c9e
PRANIT SAWANT <94974771+pranitsawant80@users.noreply.github.com>
2023-10-10 12:11:58 +0530

    Update StockMove.py

--- StockMove.py
+++ StockMove.py
@@ -4,6 +4,7 @@ import pickle
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+nltk.download('stopwords')
 
 # Load the trained logistic regression model
 model_path = 'logistic_regression_model.pkl'  # Replace with the correct file path
